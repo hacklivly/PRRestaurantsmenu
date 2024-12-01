f1f344a7e19a878aa8e96f7eec314578869ec3dd
Hacklivly <128738691+hacklivly@users.noreply.github.com>
2024-12-01 23:08:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Function to navigate to the next page
 function goToNextPage() {
-    window.location.href = "index.html"; // Replace with the actual next page URL
+    window.location.href = "https://hacklivly.github.io/PRRestaurantsmenu1/"; // Replace with the actual next page URL
 }
 
-// Function to navigate to the previous page
-function goToPreviousPage() {
-    window.location.href = "previous-page.html"; // Replace with the actual previous page URL
-}
+
